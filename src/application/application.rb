73e970487f8359961a1c86abcb5c38f296f38b6d
Steve Klise <sk@skli.se>
2012-11-30 18:06:06 -0500

    Submit application

--- src/application/application.rb
+++ src/application/application.rb
@@ -4,6 +4,24 @@ class ApplicationApp < Sinatra::Base
 
 
   post '/submit' do
-    raise params.inspect
+    @user = User.find(netid: params[:netid])
+
+    @application = @user.application || Application.new
+
+    @application.description          = params[:description]
+    @application.write_in             = params[:write_in_label]
+    @application.strengths            = params[:strengths]
+    @application.help                 = params[:help]
+    @application.url                  = params[:url]
+    @application.labels               = params[:labels]
+    @application.preferred_classmates = params[:preferred_classmates]
+
+    @user.application = @application
+
+    if @user.save
+      "success!"
+    else
+      "uhoh, go find Steve!"
+    end
   end
 end
