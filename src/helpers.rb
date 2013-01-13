2adc859cdb880e5d5a13c9b7b39c0aee6cc03180
Steve Klise <sk@skli.se>
2013-01-12 19:05:39 -0500

    Style updates

--- src/helpers.rb
+++ src/helpers.rb
@@ -3,6 +3,20 @@ module Sinatra
     helpers do
       include Rack::Utils
       alias_method :h, :escape_html
+
+      def list(collection, attribute)
+        set = []
+        collection.each do |c|
+          set.push c[attribute]
+        end
+
+        set.join(", ")
+      end
+
+      def excerpt(content)
+        words = content.split(" ")
+        words[0..30].join(" ")
+      end
     end
   end
 end
