7fe7a646d12b41b1687019d6ec80a2b916c80b63
Steve Klise <sk@skli.se>
2013-01-13 19:46:42 -0500

    Add markdown helper

--- src/helpers.rb
+++ src/helpers.rb
@@ -1,21 +1,34 @@
 module Sinatra
   class Base
+
     helpers do
       include Rack::Utils
       alias_method :h, :escape_html
 
-      def list(collection, attribute)
+      def list(collection, options)
+        return options[:default] if collection.length == 0
+
         set = []
         collection.each do |c|
-          set.push c[attribute]
+          set.push c[options[:attribute]]
         end
 
         set.join(", ")
       end
 
+      def mdown(content)
+        markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+        :autolink => true, :space_after_headers => true)
+        marked = markdown.render(content)
+        marked.to_html
+      end
+
       def excerpt(content)
-        words = content.split(" ")
-        words[0..30].join(" ")
+        words = content.split(/ /)
+
+        words[29] += " <em>...</em>" if words.length > 30
+
+        mdown(words[0..29].join(" "))
       end
     end
   end
