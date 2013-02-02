8754f024028fb662482eff9e86f52b25d13da523
Steve Klise <sk@skli.se>
2013-02-02 15:27:57 -0500

    Add documentation for announcement and assignment models

--- src/models/assignment.rb
+++ src/models/assignment.rb
@@ -4,28 +4,76 @@ class Assignment
   property :id, Serial
   property :created_at, DateTime
   property :updated_at, DateTime
+  property :active, Boolean, default: true
+  property :year, Integer, default: DateTime.now.year, writer: :private
 
   property :title, String, length: 255
   property :brief, Text
-  property :year, Integer, default: DateTime.now.year, writer: :private
-
+  property :draft, Boolean, default: true
+  property :published_at, DateTime
   property :everyone, Boolean, default: false
+
   property :user_id, Integer
+  belongs_to :user
 
   has n, :posts
   has n, :sections, through: Resource
-  belongs_to :user
 
   attr_accessor :section_ids
 
-  before :save, :add_sections
+  before :save do
+    publish
+    add_sections
+  end
 
+  #############################################################################
+  #
+  # CLASS METHODS
+  #
+  #############################################################################
+
+  # Public: Return all active resources marked as drafts, sorted in reverse
+  # chronological order by updated_at.
+  #
+  # Returns a DataMapper Collection.
+  def self.drafts
+    all(draft: true, active: true, order: :updated_at.desc)
+  end
+
+  # Public: Return all active resources marked as published, sorted in reverse
+  # chronological order by published time.
+  #
+  # Returns a DataMapper Collection.
+  def self.published
+    all(draft: false, active: true, order: :published_at.desc)
+  end
+
+  #############################################################################
+  #
+  # INSTANCE METHODS
+  #
+  #############################################################################
+
+  # Public: Instead of removing a model from the DB, mark it as inactive. This
+  # is to make deletions recoverable and make it unnecessary to detach all
+  # associations before deletion.
+  #
+  # Returns the result of the update call.
+  def delete
+    self.update(active: false)
+  end
+
+  # Public: Returns a string representing the path to this resource.
+  #
+  # Returns a string.
   def url
     "/assignments/#{self.year}/#{self.id}"
   end
 
-  # Look at sections_id attr_accessor and everyone, as well as sections to
-  # either add or remove sections.
+  private
+
+  # Private: Look at sections_id attr_accessor and everyone, as well as
+  # sections to either add or remove sections.
   def add_sections
     # If no section_ids were given a set `everyone` to true.
     if self.section_ids.nil?
@@ -52,4 +100,14 @@ class Assignment
     end
   end
 
+  # Private: Sets published date if resource is saved with draft==false. Will
+  # only set the publish date on the first save unless resource is unpublished.
+  # If a resource is a draft, unset published_at.
+  def publish
+    if self.draft == false && self.published_at.nil?
+      self.published_at = DateTime.now
+    else
+      self.published_at = nil
+    end
+  end
 end
