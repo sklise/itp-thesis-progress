53d10679d24c534cf12eb8de43f3fd8f4159663a
Steve Klise <sk@skli.se>
2013-02-15 15:08:05 -0500

    Fix /students/ raise

--- src/students/students_controller.rb
+++ src/students/students_controller.rb
@@ -9,25 +9,27 @@ class StudentsApp < Sinatra::Base
   set :views, Proc.new { File.join(root, "views") }
   set :erb, layout: :'../../views/layout'
 
-  if ENV['RACK_ENV'] == 'production'
-    set :raise_errors, Proc.new { false }
-    set :show_exceptions, false
+  if ENV['RACK_ENV'] != 'production'
+          set :raise_errors, Proc.new { false }
+          set :show_exceptions, false
 
-    error do
-      StatHat::API.ez_post_value("ERROR : #{request.fullpath}", ENV['STATHAT_EMAIL'], 1)
+          error do
+            StatHat::API.ez_post_value("ERROR", ENV['STATHAT_EMAIL'], 1)
 
-      email_body = ""
+            email_body = "#{request.request_method} : #{request.fullpath}\n\n\n"
 
-      if @current_user
-        email_body += "CURRENT_USER: #{@current_user}\n\n"
-      end
+            if @current_user
+              email_body += "CURRENT_USER: #{@current_user}\n\n"
+            end
 
-      email_body += env['sinatra.error'].backtrace.join("\n")
-      send_email("ERROR: #{request.fullpath}", email_body)
+            email_body += env['sinatra.error'].inspect + "\n\n"
 
-      erb :'../../views/error'
-    end
-  end
+            email_body += env['sinatra.error'].backtrace.join("\n")
+            send_email("ERROR: #{request.fullpath}", email_body)
+
+            erb :'../../views/error'
+          end
+        end
 
   def authenticate
     env['warden'].authenticate!
@@ -36,8 +38,6 @@ class StudentsApp < Sinatra::Base
 
   get '/' do
     authenticate
-
-    raise request.inspect
 
     @posts = Post.published.paginate(page: 1)
 
