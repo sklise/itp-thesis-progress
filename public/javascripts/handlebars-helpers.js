7c381edcfcc6872c774dc653484159e7e1aafb5c
Steve Klise <sk@skli.se>
2013-03-12 11:44:08 -0400

    Move Handlebars helpers to separate file

---
+++ public/javascripts/handlebars-helpers.js
@@ -0,0 +1,12 @@
+Handlebars.registerHelper('time', function(timestamp) {
+  return moment(timestamp).format('MMMM Do YYYY, h:mm:ss a');
+});
+
+Handlebars.registerHelper('posturl', function (t) {
+  var netid = $('body').data().netid;
+  return "/students/"+netid+"/"+t.id+"/" + t.slug;
+});
+
+Handlebars.registerHelper('announcementurl', function (t) {
+  return "/announcements/"+t.year+"/" + t.id;
+});
--- public/javascripts/post-form.js
+++ public/javascripts/post-form.js
@@ -1,20 +1,3 @@
-Handlebars.registerHelper('time', function(timestamp) {
-  return moment(timestamp).format('MMMM Do YYYY, h:mm:ss a');
-});
-
-Handlebars.registerHelper('md', function(content) {
-  if (typeof content === 'undefined') {
-    return '';
-  } else {
-    return marked(content);
-  }
-});
-
-Handlebars.registerHelper('posturl', function (t) {
-  var netid = $('body').data().netid;
-  return "/students/"+netid+"/"+t.id+"/" + t.slug;
-});
-
 //
 // MODELS
 //
