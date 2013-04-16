f89e52a5aee1585db75c50e48dec6436eef28937
Steve Klise <sk@skli.se>
2013-04-16 11:05:41 -0400

    Fix intra_emphasis in markdown issue

--- src/helpers.rb
+++ src/helpers.rb
@@ -90,7 +90,7 @@ module Sinatra
 
       def mdown(content)
         markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-        :autolink => true, :space_after_headers => true)
+        :autolink => true, :space_after_headers => true, :no_intra_emphasis => true)
         marked = markdown.render(content || "")
         Nokogiri::HTML(marked).css('body').inner_html
       end
